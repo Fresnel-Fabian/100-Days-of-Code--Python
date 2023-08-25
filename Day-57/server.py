0f06b1119587b57ee285b3eb5cd93d5a080d8539
Fresnel Fabian <fresnelfabian@gmail.com>
2023-08-25 17:34:54 +0530

    Day 57 Templating with Jinja in Flask Applications

--- Day-57/server.py
+++ Day-57/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from random import randint
 import datetime as dt
 import requests
@@ -12,7 +12,7 @@ def home_page():
     random_number = randint(0, 9)
     return render_template("index.html", num=random_number, date=dt.datetime.now().year)
 
-@app.route("/<name>")
+@app.route("/guess/<name>")
 def user(name):
     parameters = {"name": name}
     response = requests.get("https://api.genderize.io", params=parameters)
@@ -20,6 +20,13 @@ def user(name):
     response = requests.get("https://api.agify.io", params=parameters)
     age = response.json()["age"]
     return render_template("user.html", name=name, gender=gender, age=age)
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    print(f"num: {num}")
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    return render_template("blog.html", all_post = response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
