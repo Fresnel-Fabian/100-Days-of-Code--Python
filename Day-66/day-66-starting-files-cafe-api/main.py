7521005c97256c096ac24da555cc102dc81801e8
Fresnel Fabian <fresnelfabian@gmail.com>
2023-09-17 23:17:06 +0530

    Day 66 Building API with RESTful Routing

--- Day-66/day-66-starting-files-cafe-api/main.py
+++ Day-66/day-66-starting-files-cafe-api/main.py
@@ -83,7 +83,7 @@ def random():
 ## HTTP GET - Read Record
 @app.route("/all", methods=["GET"])
 def get_all():
-    result = db.session.execute(db.select(Cafe).order_by(Cafe.name)).scalars().all()
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.id)).scalars().all()
     return jsonify(cafes = [cafe.to_dict() for cafe in result])
 
 @app.route("/search", methods=["GET"])
@@ -95,10 +95,60 @@ def search():
         return jsonify(cafes = [cafe.to_dict() for cafe in all_cafes])
     return jsonify(error={"Not found": "Sorry, we don't have a cafe at that location."}), 404
 ## HTTP POST - Create Record
+@app.route("/add", methods=["POST"])
+def add():
+    data = request.form
+    new_entry = Cafe(
+        name = data.get("name"),
+        map_url = data["map_url"],
+        img_url = data["img_url"],
+        location = data["location"],
+        seats = data["seats"],
+        has_toilet = bool(data["has_toilet"]),
+        has_wifi = bool(data["has_wifi"]),
+        has_sockets = bool(data["has_sockets"]),
+        can_take_calls = bool(data["can_take_calls"]),
+        coffee_price = data["coffee_price"],
+    )
+    
+    db.session.add(new_entry)
+    db.session.commit()
+    return jsonify(response= {
+        "success": "Successfully added the new cafe."
+    })
 
 ## HTTP PUT/PATCH - Update Record
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def update_price(cafe_id):
+    coffee_price = request.args.get("coffee_price")
+    cafe = db.session.get(Cafe, cafe_id)
+    if cafe:
+        cafe.coffee_price = coffee_price
+        db.session.commit()
+        return jsonify(response= {
+            "success": "Successfully updated the price."
+        })
+    else:
+        return jsonify(error={"Not found": "Sorry a cafe with that id was not found in the database"}), 404
 
 ## HTTP DELETE - Delete Record
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api_key")
+    if api_key == "TopSecretAPIKey":
+        cafe = db.session.get(Cafe, cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={
+                "success": "Successfullly deleted the cafe."
+            })
+        return jsonify(error={
+            "Not Found": "Sorry a cafe with that id was not found in the database."
+        }), 404
+    return jsonify(error={
+        "Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."
+        }), 403
 
 
 if __name__ == '__main__':
