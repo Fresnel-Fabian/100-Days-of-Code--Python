6fb6588e6130cc62a155231b2cfa26eca178a75e
Fresnel Fabian <fresnelfabian@gmail.com>
2023-09-23 23:37:59 +0530

    update

--- Day-67/day-67-starting-files-upgraded-blog/main.py
+++ Day-67/day-67-starting-files-upgraded-blog/main.py
@@ -96,7 +96,7 @@ def edit_post(post_id):
         return redirect(url_for("show_post", post_id=post_id))
     return render_template("make-post.html", title="Edit Post", form=form)
 
-# TODO: delete_post() to remove a blog post from the database
+
 # Delete a post
 @app.route("/delete/<int:post_id>")
 def delete_post(post_id):
