848425b9baafaf392372cd5fff5b04aed5c2c1da
Fresnel Fabian <fresnelfabian@gmail.com>
2023-09-29 22:44:07 +0530

    update

--- Day-69/day-69-starting-files-blog-with-users/forms.py
+++ Day-69/day-69-starting-files-blog-with-users/forms.py
@@ -14,9 +14,19 @@ class CreatePostForm(FlaskForm):
 
 
 # TODO: Create a RegisterForm to register new users
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("SIGN ME UP!")
 
 
 # TODO: Create a LoginForm to login existing users
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    submit = SubmitField("LOG ME IN!")
+
 
 
 # TODO: Create a CommentForm so users can leave comments below posts
