48f0ef0860ddc715ea935318076cb1df30f53462
Fresnel Fabian <fresnelfabian@gmail.com>
2023-10-09 22:56:12 +0530

    update

--- Day-70 & Day-71/day-70-starting-files-blog-for-deployment/main.py
+++ Day-70 & Day-71/day-70-starting-files-blog-for-deployment/main.py
@@ -277,7 +277,7 @@ def contact():
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
-
+# send contact message using smtp to the website host
 def send_email(name, email, phone, message):
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
     with smtplib.SMTP("smtp.gmail.com") as connection:
